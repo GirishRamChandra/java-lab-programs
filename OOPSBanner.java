f9bfa868dbe1644a245107b9a76851e5cb507976
Girish <gm5680@srmist.edu.in>
2026-03-03 10:59:08 +0530

    UC2 Created separate banner assignment

---
+++ OOPSBanner.java
@@ -0,0 +1,18 @@
+/**
+ * OOPS Banner App - Use Case 2
+ * Goal: Print "OOPS" as a 7-line ASCII banner using manual print statements.
+ * @author Girish Ram Chandra
+ * @version 1.0
+ */
+public class OOPSBanner {
+    public static void main(String[] args) {
+        // Rendering OOPS using 7 lines of asterisks and spaces
+        System.out.println(" ****    ****    ******    ******");
+        System.out.println("*     * *     *  *     *   *");
+        System.out.println("*     * *     *  *     *   *");
+        System.out.println("*     * *     *  ******    ******");    
+        System.out.println("*     * *     *  *              *");
+        System.out.println("*     * *     *  *              *");
+        System.out.println(" ****   * ****   *         ******");
+    }
+}
